5272d429d3c540b084570ed9c84fd1cf719f4fc8
nadbot <Daniel.Hamandouche@gmx.net>
2020-05-08 22:10:14 +0200

    Fixed warnings

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,9 +5,10 @@ import GradeChecker
 import credentials as cred
 from tabulate import tabulate
 
-
 client = discord.Client()
 running = False
+
+
 @client.event
 async def on_message(message):
     global running
@@ -15,89 +16,98 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #TODO improve commands: commands lower and upper case working, make list clickable (show recommandations?)
-    #TODO put commands inside methods
-    #TODO reload grades only every few minutes and not with every call
+    # TODO improve commands: commands lower and upper case working, make list clickable (show recommandations?)
+    # TODO put commands inside methods
+    # TODO reload grades only every few minutes and not with every call
     if message.content.startswith('!hello'):
         await message.channel.send("Hello")
-    #simple help method
+    # simple help method
     if message.content.startswith('!help'):
         await message.channel.send("Use !getClasses to refresh the classes which will be checked\n"
-              "Use !showGrades to show all available grades in all courses\n"
-              "Use !showGrade to show a certain grade in a course. Choosing can be done by sending the corresponding number")
-    #call getClasses function, which updates list of links for grades
+                                   "Use !showGrades to show all available grades in all courses\n"
+                                   "Use !showGrade to show a certain grade in a course. "
+                                   "Choosing can be done by sending the corresponding number")
+    # call getClasses function, which updates list of links for grades
     if message.content.startswith('!getClasses'):
-        classes = GradeChecker.getClasses()
-    #show all grades
+        classes = GradeChecker.get_classes()
+        if len(classes) == 0:
+            await message.channel.send(
+                "Failed to find any classes. Please check if you have the correct module and are logged in")
+    # show all grades
     if message.content.startswith('!showGrades'):
-        grades = GradeChecker.checkGrades()
-        await message.channel.send("```"+createTableforGrades(grades)+"```")
-    #show grade for specific entry
+        grades = GradeChecker.check_grades()
+        await message.channel.send("```" + create_table_for_grades(grades) + "```")
+    # show grade for specific entry
     elif message.content.startswith('!showGrade'):
-        #update all grades
-        grades = GradeChecker.checkGrades()
+        # update all grades
+        grades = GradeChecker.check_grades()
         courses = []
         for index, key in enumerate(grades):
-            courses.append([index,key])
+            courses.append([index, key])
         print(courses)
-        #print table with course and an index
-        await message.channel.send("```Choose the course by replying with the index: \n"+str(tabulate(courses,["Index","Course"]))+"```")
-        #wait for specific index (currently does not catch invalid options
+        # print table with course and an index
+        await message.channel.send(
+            "```Choose the course by replying with the index: \n" + str(tabulate(courses, ["Index", "Course"])) + "```")
+        # wait for specific index (currently does not catch invalid options
         msg = await client.wait_for('message')
-        #find chosen course
-        courseChosen = list(grades.keys())[int(msg.content)]
-        print("You chose "+str(courseChosen))
+        # find chosen course
+        course_chosen = list(grades.keys())[int(msg.content)]
+        print("You chose " + str(course_chosen))
         items = []
-        #create list of items in that course with indices
-        for index, item in enumerate(grades[courseChosen]):
+        # create list of items in that course with indices
+        for index, item in enumerate(grades[course_chosen]):
             items.append([index, item['name'], item['score']])
         print(items)
-        #print table with assignments and index
+        # print table with assignments and index
         await message.channel.send(
-            "```Choose the item by replying with the index: \n" + str(tabulate(items, ["Index", "Item","Grade"])) + "```")
-        #wait for specific index
+            "```Choose the item by replying with the index: \n" + str(
+                tabulate(items, ["Index", "Item", "Grade"])) + "```")
+        # wait for specific index
         msg = await client.wait_for('message')
-        itemID = int(msg.content)
-        itemChosen = grades[courseChosen][itemID]
-        print("You chose " + str(itemChosen['name']))
-        #show details for that item
+        item_id = int(msg.content)
+        item_chosen = grades[course_chosen][item_id]
+        print("You chose " + str(item_chosen['name']))
+        # show details for that item
         await message.channel.send(
             "```" + str(
-                tabulate([[itemChosen['name'],itemChosen['score'],itemChosen['average'],itemChosen['median']]], ["Name", "Grade", "Average","Median"])) + "```")
-        if "-" in itemChosen['score']:
-            #if grade not available, allow user to watch that item
+                tabulate([[item_chosen['name'], item_chosen['score'], item_chosen['average'], item_chosen['median']]],
+                         ["Name", "Grade", "Average", "Median"])) + "```")
+        if "-" in item_chosen['score']:
+            # if grade not available, allow user to watch that item
             await message.channel.send("Do you want to watch this item? (yes/no)")
             msg = await client.wait_for('message')
             print("You chose " + str(msg.content))
             if msg.content == "yes":
-                #item will be observed, bot will send message once grade becomes available, checking every few minutes
-                await message.channel.send(str(itemChosen['name']) +" will be observed for changes")
+                # item will be observed, bot will send message once grade becomes available, checking every few minutes
+                await message.channel.send(str(item_chosen['name']) + " will be observed for changes")
                 channel = message.channel
-                #prevents it from running multiple instances at once
-                #TODO if observing multiple grades, make list and send updates for each
+                # prevents it from running multiple instances at once
+                # TODO if observing multiple grades, make list and send updates for each
                 if not running:
-                    client.loop.create_task(my_background_task(channel,courseChosen,itemID))
+                    client.loop.create_task(my_background_task(channel, course_chosen, item_id))
                     running = True
             else:
-                await message.channel.send(str(itemChosen['name']) + " will not be observed for changes")
+                await message.channel.send(str(item_chosen['name']) + " will not be observed for changes")
 
-def createTableforGrades(grades):
-    '''
+
+def create_table_for_grades(grades):
+    """
     Create a proper table for showing the grades. Displays for each course the items and their grade.
     :param grades: Dict of grades, gathered from GradeChecker
     :return: String representing the table. Can be sent to the user
-    '''
+    """
     string = ""
     for key in grades:
         print(key)
-        gradeForCourse = []
+        grade_for_course = []
         for item in grades[key]:
-            grade = [item['name'],item['score']]
-            gradeForCourse.append(grade)
-        print(tabulate(gradeForCourse,["Item","Grade"]))
-        string+=key+"\n"
-        string+=tabulate(gradeForCourse,["Item","Grade"])+"\n\n"
+            grade = [item['name'], item['score']]
+            grade_for_course.append(grade)
+        print(tabulate(grade_for_course, ["Item", "Grade"]))
+        string += key + "\n"
+        string += tabulate(grade_for_course, ["Item", "Grade"]) + "\n\n"
     return string
+
 
 @client.event
 async def on_ready():
@@ -106,46 +116,50 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-#TODO improve to be able to watch several grades at once
-async def my_background_task(channel, course, itemID):
-    '''
+
+# TODO improve to be able to watch several grades at once
+async def my_background_task(channel, course, item_id, time=180):
+    """
     Asynchronous method used to check regularly for updates on specific grades.
     :param channel: Channel on which update should be sent
     :param course: Course for which the grade is being observed
-    :param itemID: ID of the item that is being observed
+    :param item_id: ID of the item that is being observed
+    :param time: Time in seconds how often the background task should run
     :return: returns nothing, prints to user once it finds something
-    '''
+    """
     global running
-    #await client.wait_until_ready()
+    # await client.wait_until_ready()
     counter = 0
     print("Test")
     while True:
-        #check for updates
-        grades = GradeChecker.checkGrades()
-        observedGrade = grades[course][itemID]
-        #look if observed grade is still not available
-        if "-" not in observedGrade['score']:
-            #if it is available, stop the background task and send info to user
-            await channel.send("Your grade for "+observedGrade['name']+ " is " + str(observedGrade['score']))
+        # check for updates
+        grades = GradeChecker.check_grades()
+        observed_grade = grades[course][item_id]
+        # look if observed grade is still not available
+        if "-" not in observed_grade['score']:
+            # if it is available, stop the background task and send info to user
+            await channel.send("Your grade for " + observed_grade['name'] + " is " + str(observed_grade['score']))
             running = False
             break
         else:
-            #otherwise try again in 3 minutes
+            # otherwise try again in 3 minutes
             counter += 1
             print(counter)
-            print(observedGrade)
-            await asyncio.sleep(180) # task runs every 180 seconds
+            print(observed_grade)
+            print("Running Async task after "+str(time)+" seconds")
+            await asyncio.sleep(time)  # task runs every 180 seconds
+
 
 def main():
-    '''
-    Initial method once bot starts. Will login to blackboard and retrieve list of courses. After that, it will start the discord bot
+    """
+    Initial method once bot starts. Will login to blackboard and retrieve list of courses.
+    After that, it will start the discord bot.
     :return: Nothing
-    '''
+    """
     GradeChecker.login()
-    GradeChecker.getClasses()
+    GradeChecker.get_classes()
     client.run(cred.TOKEN)
 
 
 if __name__ == '__main__':
     main()
-
--- GradeChecker.py
+++ GradeChecker.py
@@ -4,12 +4,13 @@ import time
 
 driver = webdriver.Chrome('C:/images/chromedriver.exe')
 
+
 def login():
-    '''
+    """
     Method for logging into blackboard. Uses link and credentials from credentials.py.
     The client will stay logged in in this window until a certain amount of time expires.
     :return: Nothing
-    '''
+    """
     driver.get(cred.linkLogin)
     username = driver.find_element_by_id("user_id")
     username.send_keys(cred.ssid)
@@ -18,78 +19,85 @@ def login():
     button = driver.find_element_by_id('entry-login')
     button.click()
 
-def checkGrade(link):
-    '''
+
+def check_grade(link):
+    """
     Checks the grades for a given link
     :param link: Link of grades page for that specific course
     :return: list of grades for that course
-    '''
+    """
+    access = has_access(link)
+    if not access:
+        print("No access, logging in")
+        login()
     driver.get(link)
-    gradesWrapper = driver.find_elements_by_class_name("sortable_item_row")
-    gradesForCourse = []
+    grades_wrapper = driver.find_elements_by_class_name("sortable_item_row")
+    grades_for_course = []
     # loop through all grades
-    for row in gradesWrapper:
+    for row in grades_wrapper:
         # get wrapper for grade
         score = row.find_element_by_class_name("grade")
         # get name of assignment.
         # If it is multiple lines divide it into name, due date and type of assignment
         name = row.find_element_by_class_name("gradable").text
         if "\n" in name:
-            name, due, type = name.split("\n")
+            name, due, type_of_assignment = name.split("\n")
         # get maximum amount of points possible (usually 10)
-        pointsPossible = score.find_elements_by_class_name("pointsPossible")
-        if len(pointsPossible) > 0:
-            pointsPossible = pointsPossible[0].text
+        points_possible = score.find_elements_by_class_name("pointsPossible")
+        if len(points_possible) > 0:
+            points_possible = points_possible[0].text
         else:
             # if it is not given, use the default value of 10
-            pointsPossible = "/10"
-        testGrade = score.find_element_by_class_name("grade").text
+            points_possible = "/10"
+        test_grade = score.find_element_by_class_name("grade").text
         if score.find_elements_by_class_name("itemStats"):
             # if additional stats are given, also add store these
             # otherwise set them to 0
-            itemStats = score.find_element_by_class_name("itemStats")
-            average = itemStats.find_element_by_class_name("ave").text
-            median = itemStats.find_element_by_class_name("med").text
+            item_stats = score.find_element_by_class_name("itemStats")
+            average = item_stats.find_element_by_class_name("ave").text
+            median = item_stats.find_element_by_class_name("med").text
         else:
             average = 0
             median = 0
         # merge grade and max points to grade column
-        result = testGrade+""+pointsPossible
+        result = test_grade + "" + points_possible
         # add everything as dict to the grades from that course
-        gradesForCourse.append({'name': name, 'score': result, 'average': average, 'median': median})
+        grades_for_course.append({'name': name, 'score': result, 'average': average, 'median': median})
 
-    return gradesForCourse
+    return grades_for_course
 
-def printGrades(grades):
-    '''
+
+def print_grades(grades):
+    """
     Prints grades for a given list of grades
     :param grades: list of grades to print
     :return: Nothing
-    '''
+    """
     for key in grades:
         print(key)
         for assignment in grades[key]:
             print(assignment)
             print("\n")
 
-def getClasses():
-    '''
+
+def get_classes():
+    """
     Method to get a list of courses the user is subscribed to and the url to their grades page
     :return: Nothing, updates list from credentials
-    '''
+    """
     driver.get("https://uu.blackboard.com/")
 
     # find block with all courses
-    classWrapper = driver.find_elements_by_id("div_4_1")
+    class_wrapper = driver.find_elements_by_id("div_4_1")
     classes = []
     # if module is not loaded, wait 5 seconds and repeat
-    while "Please wait while the module loads" in classWrapper[0].text:
+    while "Please wait while the module loads" in class_wrapper[0].text:
         driver.get("https://uu.blackboard.com/")
         time.sleep(5)
-        classWrapper = driver.find_elements_by_id("div_4_1")
+        class_wrapper = driver.find_elements_by_id("div_4_1")
 
     # loop through the courses
-    for row in classWrapper:
+    for row in class_wrapper:
         # find the hyperlink, as it contains the id of the course
         courses = row.find_elements_by_tag_name("a")
         for course in courses:
@@ -102,34 +110,53 @@ def getClasses():
         # link of the course is given by href property
         link = element.get_property("href")
         # as link would lead to start page of that course, we extract id only
-        _,end = link.split("&id=")
-        id,_ = end.split("&url=")
+        _, end = link.split("&id=")
+        course_id, _ = end.split("&url=")
 
         # enter id of course in url for grades
-        url = 'https://uu.blackboard.com/webapps/bb-mygrades-bb_bb60/myGrades?course_id='+id+'&stream_name=mygrades&is_stream=false'
+        url = 'https://uu.blackboard.com/webapps/bb-mygrades-bb_bb60/myGrades?course_id=' + course_id + \
+              '&stream_name=mygrades&is_stream=false'
         # add entry to links
         cred.links[element.text] = url
     print(cred.links)
+    return cred.links
 
 
-def checkGrades():
-    '''
+def check_grades():
+    """
     Wrapper for checkGrade method. Calls it once for every course
     :return: dict of all grades for all courses
-    '''
+    """
     grades = {}
     for key in cred.links:
-        grades[key] = checkGrade(cred.links[key])
+        grades[key] = check_grade(cred.links[key])
     return grades
 
+
+def has_access(link):
+    """
+    Method to test whether the person has access to the grades. If he doesn't he should call the login function
+    :param link: Link which will be tested
+    :return: Boolean with True if he has access, otherwise false
+    """
+    driver.get(link)
+    access_denied_wrapper = driver.find_elements_by_id("bbNG.receiptTag.content")
+    for tag in access_denied_wrapper:
+        if "An error occurred." in tag.text:
+            print(tag.text)
+            return False
+    return True
+
+
 def main():
-    '''
+    """
     Main Method
     :return: Nothing
-    '''
+    """
     login()
-    grades = checkGrades()
-    printGrades(grades)
+    grades = check_grades()
+    print_grades(grades)
+
 
 if __name__ == '__main__':
     main()
